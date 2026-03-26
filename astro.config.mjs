feb05dd7a61dfbd8726138d419e4c373aa5e1d09
LeszczynskiKarol <leszczu123@wp.pl>
2026-03-26 10:36:20 +0100

    git push from local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://www.ebookcopywriting.pl',
-  integrations: [tailwind()],
-  output: 'static',
+  site: "https://www.ebookcopywriting.pl",
+  integrations: [tailwind(), sitemap()],
+  output: "static",
 });
